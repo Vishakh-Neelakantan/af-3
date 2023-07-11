d8d0ffe4a552e49fdd5b86fa97e677a46fb54dbf
kshitijagar <kshitijagarwal2808@gmail.com>
2023-07-11 11:12:43 +0530

    electives page

--- src/pages/Electives.jsx
+++ src/pages/Electives.jsx
@@ -1,9 +1,170 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { database } from '../firebaseConfig';
+import { collection, getDocs, doc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore';
 
-const Electives = () => {
+const Subjects = () => {
+  const [subjects, setSubjects] = useState([]);
+  const [newSubjectCode, setNewSubjectCode] = useState('');
+  const [newSubjectName, setNewSubjectName] = useState('');
+  const [newSubjectDescription, setNewSubjectDescription] = useState('');
+  const [editSubjectId, setEditSubjectId] = useState('');
+  const [editSubjectCode, setEditSubjectCode] = useState('');
+  const [editSubjectName, setEditSubjectName] = useState('');
+  const [editSubjectDescription, setEditSubjectDescription] = useState('');
+
+  useEffect(() => {
+    const fetchSubjects = async () => {
+      const subjectsRef = collection(database, 'subjects');
+      const snapshot = await getDocs(subjectsRef);
+      const subjectList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setSubjects(subjectList);
+    };
+
+    fetchSubjects();
+  }, []);
+
+  const addSubject = async () => {
+    if (newSubjectCode !== '' && newSubjectName !== '' && newSubjectDescription !== '') {
+      try {
+        const subjectsRef = collection(database, 'subjects');
+        const newSubject = {
+          code: newSubjectCode,
+          name: newSubjectName,
+          description: newSubjectDescription
+        };
+        await addDoc(subjectsRef, newSubject);
+
+        setNewSubjectCode('');
+        setNewSubjectName('');
+        setNewSubjectDescription('');
+        setSubjects([...subjects, newSubject]);
+      } catch (error) {
+        console.error('Error adding subject:', error);
+      }
+    }
+  };
+
+  const deleteSubject = async (subjectId) => {
+    try {
+      const subjectRef = doc(database, 'subjects', subjectId);
+      await deleteDoc(subjectRef);
+
+      const updatedSubjects = subjects.filter((subject) => subject.id !== subjectId);
+      setSubjects(updatedSubjects);
+    } catch (error) {
+      console.error('Error deleting subject:', error);
+    }
+  };
+
+  const editSubject = (subjectId, subjectCode, subjectName, subjectDescription) => {
+    setEditSubjectId(subjectId);
+    setEditSubjectCode(subjectCode);
+    setEditSubjectName(subjectName);
+    setEditSubjectDescription(subjectDescription);
+  };
+
+  const updateSubject = async () => {
+    try {
+      const subjectRef = doc(database, 'subjects', editSubjectId);
+      await updateDoc(subjectRef, {
+        code: editSubjectCode,
+        name: editSubjectName,
+        description: editSubjectDescription
+      });
+
+      const updatedSubjects = subjects.map((subject) => {
+        if (subject.id === editSubjectId) {
+          return {
+            ...subject,
+            code: editSubjectCode,
+            name: editSubjectName,
+            description: editSubjectDescription
+          };
+        }
+        return subject;
+      });
+
+      setSubjects(updatedSubjects);
+      setEditSubjectId('');
+      setEditSubjectCode('');
+      setEditSubjectName('');
+      setEditSubjectDescription('');
+    } catch (error) {
+      console.error('Error updating subject:', error);
+    }
+  };
+
   return (
-    <div>coming soon....</div>
-  )
-}
+    <div>
+      <h1>Subject Management Tool</h1>
+      <div>
+        <input
+          type="text"
+          value={newSubjectCode}
+          onChange={(e) => setNewSubjectCode(e.target.value)}
+          placeholder="Enter subject code"
+        />
+        <input
+          type="text"
+          value={newSubjectName}
+          onChange={(e) => setNewSubjectName(e.target.value)}
+          placeholder="Enter subject name"
+        />
+        <input
+          type="text"
+          value={newSubjectDescription}
+          onChange={(e) => setNewSubjectDescription(e.target.value)}
+          placeholder="Enter subject description"
+        />
+        <button onClick={addSubject}>Add Subject</button>
+      </div>
+      <ul>
+        {subjects.map((subject) => (
+          <li key={subject.id}>
+            {subject.id === editSubjectId ? (
+              <>
+                <input
+                  type="text"
+                  value={editSubjectCode}
+                  onChange={(e) => setEditSubjectCode(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editSubjectName}
+                  onChange={(e) => setEditSubjectName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editSubjectDescription}
+                  onChange={(e) => setEditSubjectDescription(e.target.value)}
+                />
+                <button onClick={updateSubject}>Save</button>
+              </>
+            ) : (
+              <>
+                <div>
+                  <strong>Code:</strong> {subject.code}
+                </div>
+                <div>
+                  <strong>Name:</strong> {subject.name}
+                </div>
+                <div>
+                  <strong>Description:</strong> {subject.description}
+                </div>
+                <button onClick={() => editSubject(subject.id, subject.code, subject.name, subject.description)}>
+                  Edit
+                </button>
+                <button onClick={() => deleteSubject(subject.id)}>Delete</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Electives
+export default Subjects;
--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,9 +1,187 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { database } from '../firebaseConfig';
+import { collection, getDocs, doc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore';
 
 const Students = () => {
-  return (
-    <div>Students</div>
-  )
-}
+  const [students, setStudents] = useState([]);
+  const [newStudentId, setNewStudentId] = useState('');
+  const [newStudentName, setNewStudentName] = useState('');
+  const [newStudentEmail, setNewStudentEmail] = useState('');
+  const [newStudentMobile, setNewStudentMobile] = useState('');
+  const [editStudentId, setEditStudentId] = useState('');
+  const [editStudentName, setEditStudentName] = useState('');
+  const [editStudentEmail, setEditStudentEmail] = useState('');
+  const [editStudentMobile, setEditStudentMobile] = useState('');
 
-export default Students
+  useEffect(() => {
+    const fetchStudents = async () => {
+      const studentsRef = collection(database, 'students');
+      const snapshot = await getDocs(studentsRef);
+      const studentList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setStudents(studentList);
+    };
+
+    fetchStudents();
+  }, []);
+
+  const addStudent = async () => {
+    if (newStudentId !== '' && newStudentName !== '' && newStudentEmail !== '' && newStudentMobile !== '') {
+      try {
+        const studentsRef = collection(database, 'students');
+        const newStudent = {
+          id: newStudentId,
+          name: newStudentName,
+          email: newStudentEmail,
+          mobile: newStudentMobile
+        };
+        await addDoc(studentsRef, newStudent);
+
+        setNewStudentId('');
+        setNewStudentName('');
+        setNewStudentEmail('');
+        setNewStudentMobile('');
+        setStudents([...students, newStudent]);
+      } catch (error) {
+        console.error('Error adding student:', error);
+      }
+    }
+  };
+
+  const deleteStudent = async (studentId) => {
+    try {
+      const studentRef = doc(database, 'students', studentId);
+      await deleteDoc(studentRef);
+
+      const updatedStudents = students.filter((student) => student.id !== studentId);
+      setStudents(updatedStudents);
+    } catch (error) {
+      console.error('Error deleting student:', error);
+    }
+  };
+
+  const editStudent = (studentId, studentName, studentEmail, studentMobile) => {
+    setEditStudentId(studentId);
+    setEditStudentName(studentName);
+    setEditStudentEmail(studentEmail);
+    setEditStudentMobile(studentMobile);
+  };
+
+  const updateStudent = async () => {
+    try {
+      const studentRef = doc(database, 'students', editStudentId);
+      await updateDoc(studentRef, {
+        id: editStudentId,
+        name: editStudentName,
+        email: editStudentEmail,
+        mobile: editStudentMobile
+      });
+
+      const updatedStudents = students.map((student) => {
+        if (student.id === editStudentId) {
+          return {
+            ...student,
+            id: editStudentId,
+            name: editStudentName,
+            email: editStudentEmail,
+            mobile: editStudentMobile
+          };
+        }
+        return student;
+      });
+
+      setStudents(updatedStudents);
+      setEditStudentId('');
+      setEditStudentName('');
+      setEditStudentEmail('');
+      setEditStudentMobile('');
+    } catch (error) {
+      console.error('Error updating student:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Student Management Tool</h1>
+      <div>
+        <input
+          type="text"
+          value={newStudentId}
+          onChange={(e) => setNewStudentId(e.target.value)}
+          placeholder="Enter student ID"
+        />
+        <input
+          type="text"
+          value={newStudentName}
+          onChange={(e) => setNewStudentName(e.target.value)}
+          placeholder="Enter student name"
+        />
+        <input
+          type="text"
+          value={newStudentEmail}
+          onChange={(e) => setNewStudentEmail(e.target.value)}
+          placeholder="Enter student email"
+        />
+        <input
+          type="text"
+          value={newStudentMobile}
+          onChange={(e) => setNewStudentMobile(e.target.value)}
+          placeholder="Enter student mobile"
+        />
+        <button onClick={addStudent}>Add Student</button>
+      </div>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.id === editStudentId ? (
+              <>
+                <input
+                  type="text"
+                  value={editStudentId}
+                  onChange={(e) => setEditStudentId(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editStudentName}
+                  onChange={(e) => setEditStudentName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editStudentEmail}
+                  onChange={(e) => setEditStudentEmail(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editStudentMobile}
+                  onChange={(e) => setEditStudentMobile(e.target.value)}
+                />
+                <button onClick={updateStudent}>Save</button>
+              </>
+            ) : (
+              <>
+                <div>
+                  <strong>ID:</strong> {student.id}
+                </div>
+                <div>
+                  <strong>Name:</strong> {student.name}
+                </div>
+                <div>
+                  <strong>Email:</strong> {student.email}
+                </div>
+                <div>
+                  <strong>Mobile:</strong> {student.mobile}
+                </div>
+                <button onClick={() => editStudent(student.id, student.name, student.email, student.mobile)}>Edit</button>
+                <button onClick={() => deleteStudent(student.id)}>Delete</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Students;
